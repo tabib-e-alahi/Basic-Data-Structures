250476b909723fc8f9d58a457827376b4fa27433
tabib-e-alahi <etabib.alahi@gmail.com>
2025-01-30 12:37:46 +0600

    doubly linked list insert at tail added

---
+++ Doubly Linked List/insert_functions/insert_at_tail.cpp
@@ -0,0 +1,67 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Node
+{
+public:
+    int val;
+    Node* next;
+    Node* prev;
+
+    Node(int val)
+    {
+        this->val = val;
+        this->next = NULL;
+        this->prev = NULL;
+    }
+};
+
+void insert_at_tail(Node* &head, Node* &tail, int val)
+{
+    Node* newNode = new Node(val);
+    if(head == NULL)
+    {
+       head = newNode;
+       tail = newNode;
+       return;
+    }
+    tail->next = newNode;
+    newNode->prev = tail;
+
+    tail = newNode;
+}
+
+void print_doubly(Node* head)
+{
+    Node* tmp = head;
+    while(tmp != NULL)
+    {
+        cout << tmp->val << " ";
+        tmp = tmp->next;
+    }
+    cout << endl;
+}
+
+int main()
+{
+    Node* head = new Node(10);
+    Node* a = new Node(20);
+    Node* b = new Node(45);
+    Node* tail = new Node(30);
+
+    head->next = a;
+    a->prev = head;
+
+    a->next = b;
+    b->prev = a;
+
+    b->next = tail;
+    tail->prev = b;
+
+    print_doubly(head);
+    insert_at_tail(head, tail, 50);
+    print_doubly(head);
+
+    return 0;
+}
+
