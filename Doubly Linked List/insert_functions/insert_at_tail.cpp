7407d97c69bdb413e8dc8dc4ae75e3b8b34d7937
tabib-e-alahi <etabib.alahi@gmail.com>
2025-01-30 12:48:41 +0600

    doubly linked list insert at any position added

--- Doubly Linked List/insert_functions/insert_at_tail.cpp
+++ Doubly Linked List/insert_functions/insert_at_tail.cpp
@@ -16,19 +16,19 @@ public:
     }
 };
 
-void insert_at_tail(Node* &head, Node* &tail, int val)
+// except head and tail for this we have functions already
+void insert_at_any_position(Node* &head, int idx, int val)
 {
     Node* newNode = new Node(val);
-    if(head == NULL)
-    {
-       head = newNode;
-       tail = newNode;
-       return;
-    }
-    tail->next = newNode;
-    newNode->prev = tail;
+    Node* tmp = head;
 
-    tail = newNode;
+    for(int i = 1; i < idx; i++){
+        tmp = tmp->next;
+    }
+    newNode->next = tmp->next;
+    tmp->next->prev = newNode;
+    tmp->next = newNode;
+    newNode->prev = tmp;
 }
 
 void print_doubly(Node* head)
@@ -59,7 +59,7 @@ int main()
     tail->prev = b;
 
     print_doubly(head);
-    insert_at_tail(head, tail, 50);
+    insert_at_any_position(head, 3, 70);
     print_doubly(head);
 
     return 0;
