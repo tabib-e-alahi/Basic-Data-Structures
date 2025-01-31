0f95833d65f395edd3465ed3c3ce220caefcc7dc
tabib-e-alahi <etabib.alahi@gmail.com>
2025-01-31 15:04:23 +0600

    STL List: capacity functions added[clear(), empty(), resize()]

---
+++ STL List/STL_capacity_functions.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    // list<int> l = {1,2,3,4,5};
+    // int a[] = {10,20,30};
+    vector<int> v = {10,20,30};
+    //also can store vector into list
+    list<int> l2(v.begin(),v.end());
+
+    // l2.clear();
+    // cout << l2.size() << endl;
+    l2.resize(5,100);
+    for(int val : l2)
+    {
+        cout << val << endl;
+    }
+    return 0;
+}
