22f816c24683ea5989cc81b3812a7a984c25d3cd
tabib-e-alahi <etabib.alahi@gmail.com>
2025-01-31 15:28:57 +0600

    STL List: operations functions: list.remove() added

---
+++ STL List/STL_List_operation_functions.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    list<int> l = {20,30,10,50,30,60,60,10};
+
+    l.remove(10); // will remove all 10 from the list;
+
+    for(int x: l)
+    {
+        cout << x << " ";
+    }
+}
