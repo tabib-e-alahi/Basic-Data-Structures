c35761b48588de14e88483b27543f7dd3d1ce75a
tabib-e-alahi <etabib.alahi@gmail.com>
2025-01-31 15:39:03 +0600

    STL List: access and iterator functions: list.front() and next() added

--- STL List/STL_List_elecment_access_and_iterator_functions.cpp
+++ STL List/STL_List_elecment_access_and_iterator_functions.cpp
@@ -5,4 +5,10 @@ int main()
     list<int> l = {20,30,10,50,30,60,60,10};
 
     cout << l.back() << endl; // return the list tail
+
+    cout << l.front() << endl; // return the list head
+
+    //access i-th element
+//    cout << *next(l.begin(), i);
+      cout << *(next(l.begin(), 3)) << endl;
 }
